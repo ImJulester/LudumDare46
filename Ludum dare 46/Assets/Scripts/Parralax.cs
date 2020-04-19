324d108dd24e5216f9bc6ca870c4f1eeab95c335
Jules <julesevers@hotmail.nl>
2020-04-19 22:33:39 +0200

    added basic background

---
+++ Ludum dare 46/Assets/Scripts/Parralax.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+
+    private float length;
+    private float startPos;
+    public Transform camera;
+    public float parralaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (camera.transform.position.x * (1-parralaxEffect));
+        float dist = (camera.transform.position.x * parralaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+
+
+    }
+}
