649abcabcb57c320b61902d18c8dcafc8229aa65
Jules <julesevers@hotmail.nl>
2020-04-20 21:01:15 +0200

    menu

---
+++ Ludum dare 46/Assets/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void MusicSliderChange(float value)
+    {
+        musicMixer.SetFloat("MusicVol", Mathf.Log10(value * 20));
+    }
+    public void VfxSliderChange(float value)
+    {
+        Debug.Log("value : " + value);
+    }
+}
