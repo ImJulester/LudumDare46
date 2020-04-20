2a60b7ec7014f79c9bf5e4dee311cc6752fe3ffa
Luuk van Eck <martyntoby@gmail.com>
2020-04-20 23:58:54 +0200

    Final Level Added

--- Ludum dare 46/Assets/Scripts/ArrowDispenser.cs
+++ Ludum dare 46/Assets/Scripts/ArrowDispenser.cs
@@ -7,7 +7,9 @@ public class ArrowDispenser : MonoBehaviour
 
     public float timeBeforeShooting = 3;
     public GameObject arrow;
+    public bool reverse;
     private float originalTime;
+    private GameObject shotArrow;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,11 @@ public class ArrowDispenser : MonoBehaviour
         if(timeBeforeShooting <= 0)
         {
             timeBeforeShooting = originalTime;
-            Instantiate(arrow, transform.position, arrow.transform.localRotation);
+            shotArrow = Instantiate(arrow, transform.position, arrow.transform.localRotation);
+            if (reverse)
+            {
+                shotArrow.transform.localScale = new Vector3(shotArrow.transform.localScale.x * -1, shotArrow.transform.localScale.y, shotArrow.transform.localScale.z);
+            }
         }
         else
         {
